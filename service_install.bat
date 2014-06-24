cd9b6f212a340ed6afbaafcad2e445e3738b4ebc
buke <wangbuke@gmail.com>
2014-06-24 14:20:56 +0800

    add script

---
+++ service_install.bat
@@ -0,0 +1,3 @@
+title GreenOdoo - www.GreenOpenERP.com
+COLOR 0A
+%CD%\runtime\bin\nssm.exe install GreenOdoo-master %CD%\start.bat
---
+++ service_remove.bat
@@ -0,0 +1,3 @@
+title GreenOdoo - www.GreenOpenERP.com
+COLOR 0A
+%CD%\runtime\bin\nssm.exe remove GreenOdoo-master confirm
